14219c299cb7a403d70409646e8595a25370c77d
Aparajit-Garg <aparajit.garg06@gmail.com>
2023-08-18 13:00:31 +0530

    Header component done

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
+import Header from "./src/components/Header";
+
+const App = () => {
+    return (
+        <Header />
+    )
+}
+
+// const appRouter = createBrowser
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render()
+root.render(<App />)
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import './../styles/Header.css'
+import { Search } from "@mui/icons-material";
+import { Home } from "@mui/icons-material";
+import { useParams } from "react-router";
+// import { Link } from react-router-dom;
+
+const Header = () => {
+    const params = useParams()
+    console.log(params)
+    return (
+        <header className="header">
+            <span className="search_input">
+                <Search />
+                <input type="text" placeholder="Search" />
+            </span>
+            <span className="home_icon">
+                 <Home />
+            </span>
+            
+        </header>
+    );
+}
+
+export default Header;
